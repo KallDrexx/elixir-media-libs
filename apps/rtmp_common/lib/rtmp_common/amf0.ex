e4bd36a78cb292b9092937f5516386529597cd0f
KallDrexx <me@mshapiro.net>
2016-05-06 21:54:18 -0400

    added serialization of amf0 objects

--- apps/rtmp_common/lib/rtmp_common/amf0.ex
+++ apps/rtmp_common/lib/rtmp_common/amf0.ex
@@ -5,6 +5,9 @@ defmodule RtmpCommon.Amf0 do
     do_deserialize(binary, [])
   end
   
+  def serialize(objects) when is_list(objects), do: do_serialize(objects, <<>>)
+  def serialize(object), do: do_serialize([object], <<>>)
+  
   defp do_deserialize(<<>>, accumulator) do
     Enum.reverse(accumulator)
   end
@@ -64,6 +67,40 @@ defmodule RtmpCommon.Amf0 do
   
   defp form_object_property({object, binary}, property_name, properties) do
     get_object_properties(binary, Map.put(properties, property_name, object))
+  end
+  
+  defp do_serialize([], binary) do
+    binary
+  end
+  
+  defp do_serialize([%RtmpCommon.Amf0.Object{type: :number, value: value} | rest], binary) do
+    do_serialize(rest, binary <> <<0::8, value::64>>)
+  end
+  
+  defp do_serialize([%RtmpCommon.Amf0.Object{type: :boolean, value: value} | rest], binary) do
+    bit = if value, do: 1, else: 0
+    
+    do_serialize(rest, binary <> <<1::8, bit::8>>)
+  end
+  
+  defp do_serialize([%RtmpCommon.Amf0.Object{type: :string, value: value} | rest], binary) do
+    length = String.length(value)
+        
+    do_serialize(rest, binary <> <<2::8, length::16>> <> value)
+  end
+  
+  defp do_serialize([%RtmpCommon.Amf0.Object{type: :object, value: properties} | rest], binary) when is_map(properties) do
+    serialized_properties = Enum.map(Map.keys(properties), fn(x) -> serialize_property(x, Map.get(properties, x)) end)
+    |> Enum.reduce(fn(x, acc) -> acc <> x end)
+        
+    do_serialize(rest, binary <> <<3>> <> serialized_properties <> <<0, 0, 9>>)
+  end
+  
+  defp serialize_property(name, object = %RtmpCommon.Amf0.Object{}) do
+    length = byte_size(name)
+    binary = <<length::16>> <> name
+    
+    do_serialize([object], binary)
   end
   
 end
--- apps/rtmp_common/test/rtmp_common/amf0/amf0_test.exs
+++ apps/rtmp_common/test/rtmp_common/amf0/amf0_test.exs
@@ -1,4 +1,4 @@
-defmodule RtmpCommon.Amf0.DeserializeTest do
+defmodule RtmpCommon.Amf0.Amf0Test do
   use ExUnit.Case, async: true
   
   test "Can deserialize number" do
@@ -32,7 +32,7 @@ defmodule RtmpCommon.Amf0.DeserializeTest do
     assert expected == RtmpCommon.Amf0.deserialize(binary)
   end
   
-  test "Can deserialize consequitive values" do
+  test "Can deserialize consecutive values" do
     binary = <<0::8, 532::64, 1::8, 1::8>>
     expected = [
       %RtmpCommon.Amf0.Object{type: :number, value: 532},
@@ -57,5 +57,65 @@ defmodule RtmpCommon.Amf0.DeserializeTest do
     }]
     
     assert expected == RtmpCommon.Amf0.deserialize(binary)
+  end
+  
+  test "Can serialize number" do
+    object = %RtmpCommon.Amf0.Object{type: :number, value: 332}
+    expected = <<0::8, 332::64>>
+    
+    assert expected == RtmpCommon.Amf0.serialize(object)
+  end
+  
+  test "Can serialize boolean" do
+    object = %RtmpCommon.Amf0.Object{type: :boolean, value: true}
+    expected = <<1::8, 1::8>>
+    
+    assert expected == RtmpCommon.Amf0.serialize(object)
+  end
+  
+  test "Can serialize UTF8-1 string" do
+    object = %RtmpCommon.Amf0.Object{type: :string, value: "test"}
+    expected = <<2::8, 4::16>> <> "test"
+    
+    assert expected == RtmpCommon.Amf0.serialize(object)
+  end
+  
+  test "Can serialize object" do
+    object = %RtmpCommon.Amf0.Object{
+      type: :object,
+      value: %{"test" => %RtmpCommon.Amf0.Object{type: :string, value: "value"}}
+    }
+    
+    expected = <<3::8, 4::16>> <> "test" <> <<2::8, 5::16>> <> "value" <> <<0, 0, 9>>
+    
+    assert expected == RtmpCommon.Amf0.serialize(object)
+  end
+  
+  test "Can serialize multiple values" do
+    objects = [
+      %RtmpCommon.Amf0.Object{type: :number, value: 532},
+      %RtmpCommon.Amf0.Object{type: :boolean, value: true}
+    ]
+    expected = <<0::8, 532::64, 1::8, 1::8>>
+    
+    assert expected == RtmpCommon.Amf0.serialize(objects)
+  end
+  
+  test "Can serialize then deserialize complex object (rtmp connect)" do
+    object = %RtmpCommon.Amf0.Object{
+      type: :object,
+      value: %{
+        "app" => %RtmpCommon.Amf0.Object{type: :string, value: "live"},
+        "type" => %RtmpCommon.Amf0.Object{type: :string, value: "nonprivate"},
+        "flashVer" => %RtmpCommon.Amf0.Object{type: :string, value: "FMLE/3.0 (compatible; FMSc/1.0)"},
+        "swfUrl" => %RtmpCommon.Amf0.Object{type: :string, value: "rtmp://169.55.8.4/live"},
+        "tcUrl" => %RtmpCommon.Amf0.Object{type: :string, value: "rtmp://169.55.8.4/live"}
+      }
+    }
+    
+    result = RtmpCommon.Amf0.serialize(object)
+      |> RtmpCommon.Amf0.deserialize
+    
+    assert [object] == result
   end
 end
