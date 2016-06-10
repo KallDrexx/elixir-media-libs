1ea42aed0a06b6643f0afdb5aa09d16d26bbe3d7
KallDrexx <me@mshapiro.net>
2016-06-09 22:31:06 -0400

    Added Dialyzer support

--- mix.exs
+++ mix.exs
@@ -21,6 +21,6 @@ defmodule Mmids.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [{:dialyxir, "~> 0.3", only: [:dev]}]
   end
 end
