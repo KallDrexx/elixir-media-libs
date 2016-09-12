0bc434b38e80d33f2bb02af6b4df644fe1bee333
KallDrexx <me@mshapiro.net>
2016-09-11 23:08:57 -0400

    Fixed list assertion macro to work with runtime lists

--- apps/rtmp_session/test/support/list_assertions.exs
+++ apps/rtmp_session/test/support/list_assertions.exs
@@ -9,20 +9,52 @@ defmodule ListAssertions.AssertionError do
 end
 
 defmodule ListAssertions do
-  defmacro contains([], match_expression) do
-    string_representation = Macro.to_string(match_expression) 
-
+  defmacro __using__(_options) do
     quote do
-      raise(ListAssertions.AssertionError, unquote(string_representation))
-    end
-  end
+      import ListAssertions
 
-  defmacro contains([head | tail], match_expression) do
-    quote do
-      case unquote(head) do
-        unquote(match_expression) -> :ok
-        _ -> ListAssertions.contains(unquote(tail), unquote(match_expression))
+      defp assert_list_contains([], _fun, expression_as_string) do
+        raise(ListAssertions.AssertionError, expression_as_string)  
+      end
+
+      defp assert_list_contains([head | tail], test_fun, expression_as_string) do
+        case test_fun.(head) do
+          true -> :ok
+          false -> assert_list_contains(tail, test_fun, expression_as_string)
+        end
       end
     end
   end
+
+  defmacro contains(list, match_expression) do
+    string_representation = Macro.to_string(match_expression)
+
+    test_fun = quote do
+      fn(item) -> 
+        case item do
+          unquote(match_expression) -> true
+          _ -> false
+        end
+      end
+    end 
+
+    quote do
+      assert_list_contains(unquote(list), unquote(test_fun), unquote(string_representation))
+    end
+  end
 end
+
+defmodule Test do
+  use ListAssertions
+
+  defstruct a: nil, b: nil
+
+  def run do
+    a = [1,%__MODULE__{a: 5, b: 7},3]
+    ListAssertions.contains(a, %__MODULE__{b: 7})
+
+    IO.puts("Match success")
+  end
+end
+
+Test.run
