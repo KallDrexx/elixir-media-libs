6fa62c2d58bd9ea0397da063b9facdb4efa0f304
KallDrexx <me@mshapiro.net>
2016-11-27 00:00:18 -0500

    Publishers can now send A/V data to players

--- apps/simple_rtmp_server/lib/simple_rtmp_server/worker.ex
+++ apps/simple_rtmp_server/lib/simple_rtmp_server/worker.ex
@@ -1,4 +1,12 @@
 defmodule SimpleRtmpServer.Worker do
+  @moduledoc """
+  Simple implementation of an RTMP server utilizing the `GenRtmpServer` behaviour.
+
+  This server allows publishing and playing data on any application and stream key.
+  This is a dumb implmenetation and thus no care is done to prevent two users from
+  publishing on the same stream key, or trying to play content on a stream key not being published to.
+  """
+
   alias RtmpSession.Events, as: RtmpEvents
   require Logger
 
@@ -6,7 +14,14 @@ defmodule SimpleRtmpServer.Worker do
 
   defmodule State do
     defstruct session_id: nil,
-              client_ip: nil
+              client_ip: nil,
+              activities: %{}
+  end
+
+  defmodule Activity do
+    defstruct type: nil, # publishing or playing
+              app_name: nil,
+              stream_key: nil
   end
 
   def start_link() do
@@ -37,27 +52,97 @@ defmodule SimpleRtmpServer.Worker do
     {:ok, state}
   end
 
-  def play_requested(%RtmpEvents.PlayStreamRequested{}, state = %State{}) do
-    {:accepted, state}
+  def play_requested(event = %RtmpEvents.PlayStreamRequested{}, state = %State{}) do
+    activity_key = generate_activity_key(event.app_name, event.stream_key)
+    case Map.fetch(state.activities, activity_key) do
+      {:ok, activity} ->
+        # Activity already exists, error if we are publishing on the same
+        # application and stream key that we are trying to publish for
+        if activity.type != :publishing do
+          message = "#{state.session_id}: Attempted to play on application (#{event.app_name}) " <>
+            "and stream key (#{event.stream_key}) that's already active with type #{activity.type}"
+
+          raise(message)
+        end
+
+        Logger.warn("#{state.session_id}: Duplicate play request for application '#{event.app_name}' " <>
+            "and stream key '#{event.stream_key}'")
+
+        {:accepted, state}
+
+      :error ->
+        # Start this activity
+        activity = %Activity{
+          type: :playing,
+          app_name: event.app_name,
+          stream_key: event.stream_key
+        }
+
+        :pg2.create(activity_key)
+        :pg2.join(activity_key, self())
+        activities = Map.put(state.activities, activity_key, activity)
+        state = %{state | activities: activities}
+
+        {:accepted, state}
+    end
   end
 
-  def play_finished(%RtmpEvents.PlayStreamFinished{}, state = %State{}) do
-    {:ok, state}
+  def play_finished(event = %RtmpEvents.PlayStreamFinished{}, state = %State{}) do
+    activity_key = generate_activity_key(event.app_name, event.stream_key)
+    case Map.fetch(state.activities, activity_key) do
+      {:ok, activity} ->
+        if activity.type != :playing do
+          message = "#{state.session_id}: Attempted to stop playing on application (#{event.app_name}) " <>
+            "and stream key (#{event.stream_key}) that's active with type #{activity.type}"
+
+            raise(message)
+        end
+
+        :pg2.leave(activity_key, self())
+        activities = Map.delete(state.activities, activity_key)
+        state = %{state | activities: activities}
+        {:ok, state}
+
+      :error ->
+        Logger.warn("#{state.session_id}: Attempted to stop playing for application '#{event.app_name}' " <>
+            "and stream key '#{event.stream_key}' but no publish activity was found")
+        {:ok, state}
+    end
   end
 
   def metadata_received(%RtmpEvents.StreamMetaDataChanged{}, state = %State{}) do
     {:ok, state}
   end
 
-  def audio_video_data_received(%RtmpEvents.AudioVideoDataReceived{}, state = %State{}) do
+  def audio_video_data_received(event = %RtmpEvents.AudioVideoDataReceived{}, state = %State{}) do
+    activity_key = generate_activity_key(event.app_name, event.stream_key)
+
+    :pg2.create(activity_key)
+    player_processes = :pg2.get_members(activity_key)
+    :ok = send_to_processes(player_processes, {:av_data, event})
+
     {:ok, state}
   end
 
-  def handle_message(_message, state = %State{}) do
+  def handle_message(message, state = %State{}) do
+    _ = Logger.debug("#{state.session_id}: Unknown message received: #{inspect(message)}")
     {:ok, state}
   end
 
   def code_change(_, state = %State{}) do
     {:ok, state}
+  end
+
+  defp generate_activity_key(app_name, stream_key) do
+    app_name <> "__" <> stream_key
+  end
+
+  defp send_to_processes([], _message) do
+    :ok
+  end
+
+  defp send_to_processes([pid | rest], message) do
+    send(pid, message)
+    send_to_processes(rest, message)
   end
 end
